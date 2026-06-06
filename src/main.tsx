6593f32c03b4e4ac9576db9ca6dcc07a112041a2
aleks <aleks7900@gmail.com>
2026-06-06 09:24:52 +0300

    proper launch

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+import ReactDOM from "react-dom/client";
+
+import { Provider } from "react-redux";
+
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
-import ReactDOM from "react-dom/client";
+
 import App from "./App.tsx";
+
+import { store } from "./store/store.ts";
 
 const queryClient =
     new QueryClient();
@@ -10,9 +16,15 @@ ReactDOM.createRoot(
     document.getElementById("root")!
 ).render(
 
-    <QueryClientProvider
-        client={queryClient}
-    >
-        <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+
+        <QueryClientProvider
+            client={queryClient}
+        >
+
+            <App />
+
+        </QueryClientProvider>
+
+    </Provider>
 );
